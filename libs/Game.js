f6be9af5c28a9122288ce17cba0d9a0a396f0aab
Attila Komlosi <komlosi.attila@gmail.com>
2015-03-15 19:38:44 +0100

    tests finished, strong refactor needed

--- libs/Game.js
+++ libs/Game.js
@@ -1,9 +1,15 @@
 var Backbone = require('backbone'),
-	$ = require('jquery');
+	$ = require('jquery'),
+	_ = require('underscore');
 Backbone.$ = $;
 module.exports = Backbone.View.extend({
 
 	_isBoardExists : false,
+	_lastClickedEl : null,
+	_isGameInProgress: false,
+
+	_oneRowDistanceCounters  : [-12, -8, 8, 12],
+	_twoRowsDistanceCounters : [-21, -19, 19, 21],
 
 	events : {
 		'click button.field' : 'onFieldClick'
@@ -15,6 +21,7 @@ module.exports = Backbone.View.extend({
 	 initialize: function() {
 		this.getBoardEl();
 		this._counter = 1;
+		this._isGameInProgress = true;
 	},
 
 	/**
@@ -36,10 +43,15 @@ module.exports = Backbone.View.extend({
 	 *
 	 */
 	 _createFields: function() {
-		var elements = '';
-		for (var i = 0; i < 100; i++) {
-			elements +=
-				'<button class="field" data-number="' + i + '" data-clicked="false"></button>'
+		var elements = '', num = 0;
+		for (var row = 1; row <=10; row++) {
+			elements += '<p class="row" data-number="' + row + '">';
+			for (var i = 1; i <= 10; i++) {
+				num++;
+				elements +=
+					'<button class="field" data-number="' + num + '" data-clicked="false"></button>'
+			}
+			elements += '</p>';
 		}
 		return elements;
 	 },
@@ -49,13 +61,141 @@ module.exports = Backbone.View.extend({
 	 */
 	 onFieldClick: function(ev) {
 		var target = this.$(ev.currentTarget);
-		if (target.data('clicked') == false) {
-			this._counter = this._counter + 1;
-			target.data('clicked', true);
+		if (target.attr('data-clicked') === 'true') {
+			return;
 		}
+		if (!this.getLastClickedEl()) {
+			target.attr('data-clicked', true);
+			this._displayStep(target);
+			this._counter = this._counter + 1;
+			this._lastClickedEl = target;
+		}
+		else if (this._isValidField(target)) {
+			target.attr('data-clicked', true);
+			this._displayStep(target);
+			this._counter = this._counter + 1;
+			this._lastClickedEl = target;
+		}
+		if (this._getPossibleFieldsAround(target).length == 0) {
+			this._endGame();
+		}
+	 },
+
+	/**
+	 *
+	 * @param field
+	 * @private
+	 */
+	_displayStep : function(field) {
+		field.html(this._counter);
+	},
+
+	/**
+	 *
+	 * @param field
+	 * @returns {boolean}
+	 * @private
+	 */
+	 _isValidField: function(field) {
+		var isValidField = true,
+			possibleFields =  this._getPossibleFieldsAround(this.getLastClickedEl());
+		if (possibleFields.length == 0) {
+			this._endGame();
+		}
+		if (this.getLastClickedEl()) {
+			isValidField = possibleFields.indexOf(field.attr('data-number')) != -1;
+		}
+		return isValidField;
+	 },
+
+	/**
+	 *
+	 * @param field
+	 * @returns {Array.<T>|string|*}
+	 * @private
+	 */
+	_getPossibleFieldsAround : function(field) {
+		var fieldNumber = parseInt(field.attr('data-number')), possibleFieldEl, num,
+			oneRowArray = _.map(this._oneRowDistanceCounters, _.bind(function(i) {
+			num = fieldNumber + i;
+			possibleFieldEl = this.$('button[data-number=' + num + ']');
+			if (num > 0 &&
+				this._isOneRowDistance(field, possibleFieldEl) &&
+				possibleFieldEl.attr('data-clicked') === 'false'
+			) {
+				possibleFieldEl.addClass('hilit');
+				return possibleFieldEl.attr('data-number');
+			}
+		}, this));
+		var twoRowsArray = _.map(this._twoRowsDistanceCounters, _.bind(function(i) {
+			num = fieldNumber + i;
+			possibleFieldEl = this.$('button[data-number=' + num + ']');
+			if (num > 0 &&
+				this._isTwoRowsDistance(field, possibleFieldEl) &&
+				possibleFieldEl.attr('data-clicked') === 'false'
+			) {
+				possibleFieldEl.addClass('hilit');
+				return possibleFieldEl.attr('data-number');
+			}
+		}, this));
+		return _.compact(oneRowArray.concat(twoRowsArray));
+	},
+
+	/**
+	 *
+	 * @param field1
+	 * @param field2
+	 * @returns {boolean}
+	 * @private
+	 */
+	_areInTheSameRow: function(field1, field2) {
+		return this._getRowNumber(field1) == this._getRowNumber(field2);
+	},
+
+	/**
+	 *
+	 * @param field1
+	 * @param field2
+	 * @returns {boolean}
+	 * @private
+	 */
+	_isOneRowDistance: function(field1, field2) {
+		return Math.abs(this._getRowNumber(field2) - this._getRowNumber(field1)) == 1;
+	 },
+	/**
+	 *
+	 * @param field1
+	 * @param field2
+	 * @returns {boolean}
+	 * @private
+	 */
+	_isTwoRowsDistance: function(field1, field2) {
+		return Math.abs(this._getRowNumber(field2) - this._getRowNumber(field1)) == 2;
+	 },
+
+	/**
+	 *
+	 */
+	 _getRowNumber: function(field) {
+		return parseInt(field.parent('p.row').attr('data-number'));
+	 },
+
+	/**
+	 *
+	 */
+	 getLastClickedEl: function() {
+		return this._lastClickedEl;
 	 },
 
 	getCounterState: function() {
 		return this._counter;
+	},
+
+	_endGame: function() {
+		this._isGameInProgress = false;
+	},
+
+	isGameInProgress : function() {
+		return this._isGameInProgress;
 	}
 });
--- tests/GameTest.js
+++ tests/GameTest.js
@@ -13,8 +13,12 @@ suite('Test game', function() {
 		this.game = new GameView({
 			el: $('#sandbox')
 		});
-		validFieldIndexes = function() {
-			return []
+		this.stepIterationDataProvider = function() {
+			return [
+				1, 22, 41, 62, 81, 93, 72, 51, 32, 11, 23, 31, 52,
+				71, 92, 73, 61, 53, 34, 13, 21, 33, 12, 4, 25, 6,
+				14, 2
+			];
 		}
 	});
 	teardown(function() {
@@ -36,28 +40,57 @@ suite('Test game', function() {
 		assert.isTrue(this.game.getBoardEl().find('button').length == 100, 'it should be true');
 	});
 
+	test('ordered to exactly 10 rows', function() {
+		assert.isTrue(this.game.getBoardEl().find('p.row').length == 10);
+	});
+
 	test('board elements are clickable', function() {
 		this.game.getBoardEl().find('button')[0].click();
 		assert.isTrue(this.game.onFieldClick.calledOnce);
 	});
 
-	test('click counter starts from 1 at the beginning of the game', function() {
+	test('click counter starts from 1 in the beginning of the game', function() {
 		assert.strictEqual(this.game.getCounterState(), 1);
+	});
+
+	test('in the beginning the last clicked element is null', function() {
+		assert.isNull(this.game.getLastClickedEl());
+	});
+
+	test('can retrieve with the last clicked element', function() {
+		var testButton = this.game.getBoardEl().find('button[data-number="10"]');
+		testButton.click();
+		assert.strictEqual(testButton.attr('data-number'), this.game.getLastClickedEl().attr('data-number'));
+		//testButton = $('#game-board').find('button[data-number="29"]');
+		//testButton.click();
+		//assert.strictEqual(testButton.attr('data-number'), this.game.getLastClickedEl().attr('data-number'));
+		//testButton = $('#game-board').find('button[data-number="30"]');
+		//testButton.click();
+		//assert.strictEqual(this.game.getLastClickedEl().attr('data-number'), 29);
+		//var testButton = $('#game-board').find('button[data-number="8"]');
+		//testButton.click();
+		//assert.strictEqual(testButton.attr('data-number'), this.game.getLastClickedEl().attr('data-number'));
+	});
+
+	test('buttons can be selected by data-number value', function() {
+		var board = $('#game-board');
+		assert.strictEqual(board.find('button[data-number="10"]').length, 1);
+		assert.strictEqual(board.find('button[data-number="100"]').length, 1);
+		assert.strictEqual(board.find('button[data-number="101"]').length, 0);
 	});
 
 	test('increases counters when clicked', function() {
 		var buttonList = $('#game-board').find('button');
+		assert.strictEqual(this.game.getCounterState(), 1);
 		buttonList[0].click();
 		assert.strictEqual(this.game.getCounterState(), 2);
-		buttonList[1].click();
-		assert.strictEqual(this.game.getCounterState(), 3);
 	});
 
 	test('clicked field gets marked', function() {
 		var field = $(this.game.getBoardEl().find('button')[0]);
-		assert.isFalse(field.data('clicked'));
+		assert.isFalse(field.attr('data-clicked') == 'true');
 		field.click();
-		assert.isTrue(field.data('clicked'));
+		assert.isTrue(field.attr('data-clicked') == 'true');
 	});
 
 	test('clicked field cannot be clicked again', function() {
@@ -68,7 +101,30 @@ suite('Test game', function() {
 		assert.strictEqual(this.game.getCounterState(), 2);
 	});
 
-	test('just valid field are clickable', function() {
-		this.game.getBoardEl().find('button')[0].click();
+	test('just valid fields are clickable', function() {
+		var board = $('#game-board');
+		board.find('button[data-number="1"]').click();
+		assert.strictEqual(this.game.getCounterState(), 2);
+		board.find('button[data-number="2"]').click();
+		assert.strictEqual(this.game.getCounterState(), 2);
+		board.find('button[data-number="100"]').click();
+		assert.strictEqual(this.game.getCounterState(), 2);
+		board.find('button[data-number="13"]').click();
+		assert.strictEqual(this.game.getCounterState(), 3);
+	});
+
+	test('correct step displayed by number', function() {
+		var testButton = $('#game-board').find('button[data-number="1"]');
+		testButton.click();
+		assert.strictEqual(testButton.html(), '1');
+	});
+
+	test('game ends if there is no more step', function() {
+		var board = $('#game-board'),
+			steps = this.stepIterationDataProvider();
+		for (var i = 0; i < steps.length; i++) {
+			board.find('button[data-number="' + steps[i] + '"]').click();
+		}
+		assert.isFalse(this.game.isGameInProgress());
 	});
 });
